b24b94983ade5915f08da13a32a1a7224e511db2
James Boyd <121244615+jamestylerboyd@users.noreply.github.com>
2023-05-12 08:45:27 -0400

    Setting _app.tsx back live

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,66 +4,12 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 import { appWithTranslation } from 'next-i18next';
 import type { AppProps } from 'next/app';
 import { Inter } from 'next/font/google';
-@@ -10,6 +9,66 @@ import '@/styles/globals.css';
+
+import '@/styles/globals.css';
+
 const inter = Inter({ subsets: ['latin'] });
 
 function App({ Component, pageProps }: AppProps<{}>) {
-  const isSiteDown = true; // Set this flag to true when the site is down
-
-  if (isSiteDown) {
-    return (
-      <div className={inter.className}>
-        <div className="black-screen">
-          <div className="text-animation">
-            <h1>Eden is hard at work...</h1>
-            <h2>We will be back shortly...</h2>
-          </div>
-        </div>
-
-        <style jsx>{`
-          .black-screen {
-            background-color: black;
-            height: 100vh;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-          }
-          .text-animation {
-            font-family: "Courier New", monospace;
-            color: white;
-            text-align: center;
-            white-space: nowrap;
-          }
-          .text-animation h1,
-          .text-animation h2 {
-            animation: typing 2s steps(40), blink 1s infinite step-end;
-            overflow: hidden;
-            border-right: 2px solid white;
-            margin: 0;
-            padding: 0 0.5rem;
-          }
-          @keyframes typing {
-            from {
-              width: 0;
-            }
-            to {
-              width: 100%;
-            }
-          }
-          @keyframes blink {
-            from,
-            to {
-              border-color: transparent;
-            }
-            50% {
-              border-color: white;
-            }
-          }
-        `}</style>
-      </div>
-    );
-  }
-
   const queryClient = new QueryClient();
 
   return (
@@ -75,4 +21,5 @@ function App({ Component, pageProps }: AppProps<{}>) {
     </div>
   );
 }
+
 export default appWithTranslation(App);
